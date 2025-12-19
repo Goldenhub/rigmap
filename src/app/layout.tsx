e7f484587532b8759e71d1243835f496d25154f6
Goldenhub <goldenazubuike@gmail.com>
2025-12-19 01:50:45 +0100

    feat: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SiteHeader } from "@/components/site-header";
+import { Analytics } from "@vercel/analytics/next"
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +33,7 @@ export default function RootLayout({
         <SiteHeader />
         <main className="min-h-screen">
           {children}
+          <Analytics />
         </main>
       </body>
     </html>
